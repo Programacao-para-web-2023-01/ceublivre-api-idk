b89135c3fa7082983203d6aebcabbe64dfc9f03e
Lucas Pinheiro <lucas.gpinheiro2002@gmail.com>
2023-04-13 16:49:28 -0300

    Change Hono strict mode

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { priorities } from "~/routes/priorities";
 
-const app = new Hono();
+const app = new Hono({strict: false});
 
 app.get("/", c => c.text(":)"));
 
