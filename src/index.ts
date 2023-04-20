b93cb38d1a7ed72837e92f38e556401256d4fe6f
Lucas Pinheiro <lucas.gpinheiro2002@gmail.com>
2023-04-20 16:58:41 -0300

    Add prettyJSON and homepage visual

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from "hono";
-import { priorities } from "~/routes/priorities";
+import { priorities } from "./routes/priorities";
 import { tickets } from "./routes/tickets";
+import { prettyJSON } from "hono/pretty-json";
+import { homepage } from "homepage";
 
 const app = new Hono({ strict: false });
 
-app.get("/", (c) => c.text(":)"));
+app.use("*", prettyJSON({ space: 4 }));
+app.get("/", (c) => c.html(homepage));
 app.route("/priorities", priorities);
 app.route("/tickets", tickets);
 
