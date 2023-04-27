71577532fd74771cafac7e72d43b20d0324164bd
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-04-27 16:19:44 -0300

    Update JWT middleware

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,16 @@ import { jwt } from "hono/jwt";
 import { priorities } from "./routes/priorities";
 import { tickets } from "./routes/tickets";
 import { prettyJSON } from "hono/pretty-json";
-import { homepage } from "homepage";
+import { Bindings } from "./app";
+import { homepage } from "./routes/homepage";
 
-const app = new Hono({ strict: false });
+const app = new Hono<{ Bindings: Bindings }>({ strict: false });
 
 app.use("*", prettyJSON({ space: 2 }));
-app.use("*", jwt({ secret: "secret" }));
+app.use("*", (c, next) => {
+  const jwtMiddleware = jwt({ secret: c.env.SECRET });
+  return jwtMiddleware(c, next);
+});
 
 app.get("/", c => c.html(homepage));
 app.route("/priorities", priorities);
