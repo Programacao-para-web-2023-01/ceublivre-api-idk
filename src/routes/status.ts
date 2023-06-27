0bd60b091fea61e6a19ac499fa1a767a90b739b4
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-06-27 19:02:47 -0300

    Add get status by id endpoint

--- src/routes/status.ts
+++ src/routes/status.ts
@@ -5,6 +5,7 @@ import { Hono } from "hono";
 
 export const statusRouter = new Hono<{ Bindings: Bindings }>();
 
+// GET All statuses
 statusRouter.get("/", async c => {
   const statuses = Status.array().parse(
     (await c.env.DB.prepare("SELECT * FROM Status").all()).results
@@ -13,5 +14,27 @@ statusRouter.get("/", async c => {
   return ApiResponse.success({
     c,
     data: statuses,
+  });
+});
+
+// GET Status by id
+statusRouter.get("/:id", async c => {
+  const { id } = c.req.param();
+
+  const status = Status.safeParse(
+    await c.env.DB.prepare("SELECT * FROM Status WHERE id = ?").bind(id).first()
+  );
+
+  if (!status.success) {
+    return ApiResponse.error({
+      c,
+      status: 404,
+      error: new Error("Status não encontrado"),
+    });
+  }
+
+  return ApiResponse.success({
+    c,
+    data: status.data,
   });
 });
